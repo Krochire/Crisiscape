73b5b54b2be0e4fc8ae70b8c0aa52b9e28b1e226
Krochire <marco02cheri@gmail.com>
2026-03-12 00:50:02 +0100

    Doesn't work lmao, will maybe come back later (won't)

--- crisis.py
+++ crisis.py
@@ -10,30 +10,40 @@ class App(Tk):
     def __init__(self):
         super().__init__()
         self.attributes("-fullscreen", True)
+        self.questions = self._load_questions()
+        self.current_num = 0                                             #Selects the first question by default
+        self.current_str = StringVar(self, self.questions[0]["question_str"]) #
+        self.validate_str = StringVar(self, "") #This string holds the value used by validate_lbl
+        self._build_ui()
+        self.randomize_bg()
 
-        #Question select widget
+    def _load_questions(self):
         with open("questions.json", "r", encoding="utf-8") as file:
             question_data = json.load(file)
             #Case-insensitive btw :3c
-            self.questions = [
+            return [
                 {
                 "question_str": q["question_str"],
                 "answer": q["answer"],
                 "result": q["result"],
                 "completed": False,
                 }
-                for q in question_data
+              for q in question_data
             ]
-        
-        self.current_num = 0                                             #Selects the first question by default
-        self.current_str = StringVar(self, questions[0]["question_str"]) #
 
-        self.button_frm = Frame(self)
-        button_frm.pack(side=BOTTOM)
+    def _build_ui(self):
+        self._build_question_select()
+        self._build_question_labels()
+        self._build_answer_widget()
+
         
-        question_btn = []
-        for i in range(len(questions)):
-            question_btn.append(Button(self.button_frm,
+    def _build_question_select(self):
+        self.button_frm = Frame(self)
+        self.button_frm.pack(side=BOTTOM)
+        
+        self.question_btn = []
+        for i in range(len(self.questions)):
+            self.question_btn.append(Button(self.button_frm,
                 text=str(i+1),
                 font="Arial, 25 bold",
                 bg="darkorange",
@@ -45,72 +55,97 @@ class App(Tk):
                 highlightbackground="black",
                 highlightthickness=3
             ))
-           question_btn[i].pack(
+            self.question_btn[i].pack(
                 side=LEFT,
                 padx=20,
                 pady=(0, 50)
             )
 
-        question_lbl = Label(self, textvariable=self.current_str, font="Helvetica, 30 bold", anchor="center")
-        question_lbl.pack(side=TOP, pady=(150, 0))
+    def _build_question_labels(self):
+        self.question_lbl = Label(self, textvariable=self.current_str, font="Helvetica, 30 bold", anchor="center")
+        self.question_lbl.pack(side=TOP, pady=(150, 0))
+        
+        self.result_lbl = Label(self, text="", font="Helvetica, 24 italic",anchor="center") #TODO: is the empty textreally useful here ?
+        self.result_lbl.pack()
 
-        self.randomize_bg()
+    def _build_answer_widget(self):
+        self.valid_img = PhotoImage(file="valid.png")
+        
+        self.answer_frm = Frame(self)
+        self.answer_frm.pack(side=TOP)
+
+        self.answer_ent = Entry(self.answer_frm, font="Arial, 22", justify=CENTER, fg="white", bg="black", highlightthickness=0, borderwidth=5, insertbackground="grey")
+        self.answer_ent.pack( side=LEFT, padx=(40, 0))
+
+        self.validate_btn = Button(self.answer_frm,
+            image=self.valid_img,
+            font="Arial, 26",
+            relief="ridge",
+            borderwidth=7,
+            bg="darkred",
+            activebackground="red",
+            command=self.validate,
+            state="normal"
+        )
+        self.validate_btn.pack(side=RIGHT, pady=20, padx=(10, 20))
+
+        self.validate_lbl = Label(self, textvariable=self.validate_str, font="Arial, 22 italic")
+        self.validate_lbl.pack( side=TOP, pady=30)
 
     #Callback function to randomize the background color of all the elements
     def randomize_bg(self):
-        #the hell is this built-in function <- it returns a hexadecimal string of the number you passed it
-        ran_color = hex(random.randint(0, 4194303) #The magic number is 0xFFFFFF in decimal, as random.randint can only use decimal
+        #the hell is this built-in function <- it returns a hexadecimal string of the number you passed
+        ran_color = hex(random.randint(0, 4194303)) #The magic number is 0xFFFFFF in decimal, as random.randint can only use decimal
         bg_color = "#" + ran_color[2:].zfill(6)    #The bg attribute of tkinter requires colors in the form of #000A1E
-        self.configure(bg=background_color)
-        self.button_frm.config(bg=background_color)
-        question_lbl.config(bg=background_color)
+        
+        self.configure(bg=bg_color)
+        self.button_frm.config(bg=bg_color)
+        self.question_lbl.config(bg=bg_color)
+        self.result_lbl.config(bg=bg_color)
+        self.answer_frm.config(bg=bg_color)
+        self.validate_btn.config(highlightbackground=bg_color)
+        self.validate_lbl.config(bg=bg_color)
 
-def randomize_bg():
-    validate_lbl.config(bg=background_color)
-    answer_frm.config(bg=background_color)
-    validate_btn.config(highlightbackground=background_color)
-    result_lbl.config(bg=background_color)
+    #Callback function to change the current question to the selected one
+    def switch(self, num):
+        self.current_str.set(self.questions[num]["question_str"])
+        
+        self.question_btn[current_num].config(state="normal")
+        self.question_btn[current_num].config(relief=RAISED)
+        
+        self.current_num = num
+        
+        self.question_btn[current_num].config(state="disabled")
+        self.question_btn[current_num].config(relief=SUNKEN)
+        
+        self.validate_str.set("")
+        
+        self.answer_ent.delete(0, END)
+        
+        self.randomize_bg()
+        
+        if questions[current_num]["completed"]:
+            self.validate_btn.config(state="disabled")
+            self.result_lbl.config(text=questions[current_num]["result"])
+        else:
+            self.validate_btn.config(state="active")
+            self.result_lbl.config(text="")
 
-#Validate the answer to the question
-def validate():
-    global current_num
-    global current_str
-    if answer_ent.get().casefold() == questions[current_num].answer.casefold():
-        questions[current_num].completed=True
-        result_lbl.config(text=questions[current_num].result)
-        validate_str.set("Correct")
-        validate_lbl.config(fg="darkgreen")
-        validate_btn.config(state="disabled")
-        question_btn[current_num].config(bg="#00ff00")
-    else:
-        validate_str.set("Faux")
-        validate_lbl.config(fg="red")
-
-#Change the current question to the selected one
-def switch(num):
-    global current_str
-    global current_num
-    current_str.set(questions[num].question_str)
-    question_btn[current_num].config(state="normal")
-    question_btn[current_num].config(relief=RAISED)
-    current_num = num
-    question_btn[current_num].config(state="disabled")
-    question_btn[current_num].config(relief=SUNKEN)
-    validate_str.set("")
-    answer_ent.delete(0, END)
-    randomize_bg()
-    #Enable or disable the validate button and the result label
-    if questions[current_num].completed:
-        validate_btn.config(state="disabled")
-        result_lbl.config(text=questions[current_num].result)
-    else:
-        validate_btn.config(state="active")
-        result_lbl.config(text="")
-
-ran_color=hex(random.randint(0, 16777215))
+    #Validate the answer to the question
+    def validate():
+        if self.answer_ent.get().casefold() == questions[current_num]["answer"].casefold():
+            self.questions[current_num]["completed"]=True
+            self.result_lbl.config(text=questions[current_num].result)
+            self.validate_str.set("Correct")
+            self.validate_lbl.config(fg="darkgreen")
+            self.validate_btn.config(state="disabled")
+            self.question_btn[current_num].config(bg="#00ff00")
+        else:
+            self.validate_str.set("Faux")
+            self.validate_lbl.config(fg="red")
 
 app = App()
-
+app.mainloop()
 
 '''
 #Create the window that will contain the progress bar
@@ -153,121 +188,6 @@ for i in range(len(question)):
         fill=list_colors[i]
     )
 #progress_cnv.create_rectangle(1, 1, int(progress_cnv.winfo_width()/len(question)), progress_cnv.winfo_height()-2, fill="green");
+
+progress.mainloop()
 '''
-#The current selected question
-
-valid_img = PhotoImage(file="valid.png")
-
-#The Correct or Faux string
-validate_str = StringVar(win, "")
-
-#Container for all the question selection button
-button_frm = Frame(
-    win,
-    bg="#"+ran_color[2:].zfill(6)
-)
-button_frm.pack(side=BOTTOM)
-
-#Element that shows the question
-question_lbl = Label(
-    win,
-    textvariable=current_str,
-    bg="#"+ran_color[2:].zfill(6),
-    font="Helvetica, 30 bold",
-    anchor="center"
-)
-question_lbl.pack(
-    side=TOP,
-    pady=(150, 0)
-)
-
-#Element that shows Correct or Faux
-validate_lbl = Label(
-    win,
-    textvariable=validate_str,
-    bg="#"+ran_color[2:].zfill(6),
-    font="Arial, 22 italic"
-)
-validate_lbl.pack(
-    side=TOP,
-    pady=30
-)
-
-#Container for the entry and the validate button
-answer_frm = Frame(
-    win,
-    bg="#"+ran_color[2:].zfill(6)
-)
-answer_frm.pack(side=TOP)
-
-#You can enter the answer here
-answer_ent = Entry(
-    answer_frm,
-    font="Arial, 22",
-    justify=CENTER,
-    fg="white",
-    bg="black",
-    highlightthickness=0,
-    borderwidth=5,
-    insertbackground="grey"
-)
-answer_ent.pack(
-    side=LEFT,
-    padx=(40, 0)
-)
-
-#Confirm the answer
-validate_btn = Button(
-    answer_frm,
-    #text="Valider",
-    image=valid_img,
-    font="Arial, 26",
-    relief="ridge",
-    borderwidth=7,
-    bg="darkred",
-    activebackground="red",
-    highlightbackground="#"+ran_color[2:].zfill(6),
-    command=validate,
-    state="normal"
-)
-validate_btn.pack(
-    side=RIGHT,
-    pady=20,
-    padx=(10, 20)
-)
-
-#Label for the question result
-result_lbl = Label(
-    win,
-    text="",
-    bg="#"+ran_color[2:].zfill(6),
-    font="Helvetica, 24 italic",
-    anchor="center"
-)
-result_lbl.pack()
-
-#Generate question selection button
-for i in range(len(questions)):
-    question_btn[i] = Button(
-        button_frm,
-        text=str(i+1),
-        font="Arial, 25 bold",
-        bg="darkorange",
-        relief=RAISED,
-        activebackground="yellow",
-        command=lambda i=i: switch(i),
-        width=3,
-        borderwidth=5,
-        highlightbackground="black",
-        highlightthickness=3
-    )
-    question_btn[i].pack(
-        side=LEFT,
-        padx=20,
-        pady=(0, 50)
-    )
-
-randomize_bg()
-#The mainloop idk
-app.mainloop()
-#progress.mainloop()
